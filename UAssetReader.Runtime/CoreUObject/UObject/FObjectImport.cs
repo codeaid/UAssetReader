10ade6cf6eb2bfbefa22b721162160290abbce70
Codeaid <codeaid@users.noreply.github.com>
2022-11-21 17:00:21 +0000

    Added FObjectImport struct

---
+++ UAssetReader.Runtime/CoreUObject/UObject/FObjectImport.cs
@@ -0,0 +1,26 @@
+using UAssetReader.Runtime.Core.Misc;
+
+namespace UAssetReader.Runtime.CoreUObject.UObject;
+
+public readonly struct FObjectImport
+{
+    /// <summary>
+    /// The name of the class for the UObject represented by this resource.
+    /// </summary>
+    public FName ClassName { get; init; }
+
+    /// <summary>
+    /// The name of the package that contains the class of the UObject represented by this resource.
+    /// </summary>
+    public FName ClassPackage { get; init; }
+
+    /// <summary>
+    /// Reference to the name of the current import.
+    /// </summary>
+    public FName ObjectName { get; init; }
+
+    /// <summary>
+    /// Index into SourceLinker's ExportMap for the export associated with this import's UObject.
+    /// </summary>
+    public FPackageIndex SourceIndex { get; init; }
+}
